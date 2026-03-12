d131d091b0b76fc75576f81fe44deb2d94847673
MIGUEL VALLIM BARBIERI <miguel.barbieri3@senaisp.edu.br>
2026-03-12 10:31:41 -0300

    meu commit arrumando branch 6

---
+++ atividade6.js
@@ -0,0 +1,50 @@
+const inputTarefa = document.getElementById("inputTarefa");
+const btnAdicionar = document.getElementById("btnAdicionar");
+const listaTarefas = document.getElementById("listaTarefas");
+const mensagem = document.getElementById("mensagem");
+
+btnAdicionar.addEventListener("click", () => {
+
+    const textoTarefa = inputTarefa.value;
+
+    if(textoTarefa.trim() === ""){
+
+        mensagem.innerHTML = ` <div class="alert alert-danger"> Tarefa vazia! </div>
+        `;
+
+    } else {
+
+      
+        const novaTarefa = document.createElement("li");
+        novaTarefa.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+
+     
+        const texto = document.createElement("span");
+        texto.innerText = textoTarefa;
+
+      
+        const btnRemover = document.createElement("button");
+        btnRemover.innerText = "Remover";
+        btnRemover.classList.add("btn", "btn-danger", "btn-sm");
+
+       
+        btnRemover.addEventListener("click", () => {
+            novaTarefa.remove();
+        });
+
+        
+        novaTarefa.appendChild(texto);
+        novaTarefa.appendChild(btnRemover);
+
+       
+        listaTarefas.appendChild(novaTarefa);
+
+    
+        inputTarefa.value = "";
+
+        mensagem.innerHTML = `
+        <div class="alert alert-success">Tarefa adicionada com sucesso!</div>`;
+    }
+
+});
+
