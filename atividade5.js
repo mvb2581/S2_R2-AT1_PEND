f1647d92bb55477f31be68121329595ea380f5bb
MIGUEL VALLIM BARBIERI <miguel.barbieri3@senaisp.edu.br>
2026-03-12 10:15:33 -0300

    meu quinto commit

--- atividade5.js
+++ atividade5.js
@@ -9,47 +9,41 @@ btnAdicionar.addEventListener("click", () => {
 
     if(textoTarefa.trim() === ""){
 
-        mensagem.innerHTML = `
-        <div class="alert alert-danger">
-        Tarefa vazia!
-        </div>
+        mensagem.innerHTML = ` <div class="alert alert-danger"> Tarefa vazia! </div>
         `;
 
     } else {
 
-        // cria o li
+      
         const novaTarefa = document.createElement("li");
         novaTarefa.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
 
-        // texto da tarefa
+     
         const texto = document.createElement("span");
         texto.innerText = textoTarefa;
 
-        // botão remover
+      
         const btnRemover = document.createElement("button");
         btnRemover.innerText = "Remover";
         btnRemover.classList.add("btn", "btn-danger", "btn-sm");
 
-        // ação de remover
+       
         btnRemover.addEventListener("click", () => {
             novaTarefa.remove();
         });
 
-        // adiciona texto e botão dentro do li
+        
         novaTarefa.appendChild(texto);
         novaTarefa.appendChild(btnRemover);
 
-        // adiciona na lista
+       
         listaTarefas.appendChild(novaTarefa);
 
-        // limpa input
+    
         inputTarefa.value = "";
 
         mensagem.innerHTML = `
-        <div class="alert alert-success">
-        Tarefa adicionada com sucesso!
-        </div>
-        `;
+        <div class="alert alert-success">Tarefa adicionada com sucesso!</div>`;
     }
 
 });
