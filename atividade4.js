608e0d19b6611e0da7e0439c7ff53a4b7aa80dd2
MIGUEL VALLIM BARBIERI <miguel.barbieri3@senaisp.edu.br>
2026-03-12 10:01:11 -0300

    meu quarto commit

---
+++ atividade4.js
@@ -0,0 +1,37 @@
+const inputTarefa = document.getElementById("inputTarefa");
+const btnAdicionar = document.getElementById("btnAdicionar");
+const listaTarefas = document.getElementById("listaTarefas");
+const mensagem = document.getElementById("mensagem");
+
+btnAdicionar.addEventListener("click", () => {
+
+    const textoTarefa = inputTarefa.value;
+
+    if(textoTarefa.trim() === ""){
+
+        mensagem.innerHTML = `
+        <div class="alert alert-danger">
+            Tarefa vazia!
+        </div>
+        `;
+
+    } else {
+
+        const novaTarefa = document.createElement("li");
+
+        novaTarefa.classList.add("list-group-item");
+
+        novaTarefa.innerText = textoTarefa;
+
+        listaTarefas.appendChild(novaTarefa);
+
+        inputTarefa.value = "";
+
+        mensagem.innerHTML = `
+        <div class="alert alert-success">
+            Tarefa adicionada com sucesso!
+        </div>
+        `;
+    }
+
+});
