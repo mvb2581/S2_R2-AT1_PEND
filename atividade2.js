2d2099702498143415545415e5134b791eb9ca5d
MIGUEL VALLIM BARBIERI <miguel.barbieri3@senaisp.edu.br>
2026-03-19 08:07:58 -0300

    correção da branch 6, removendo os arquivos das atividades anteriores

--- atividade2.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById("btnMudar");
-const texto = document.getElementById("texto");
-
-botao.addEventListener("click", () => {
-    texto.innerText = "Texto alterado com JavaScript!";
-});
